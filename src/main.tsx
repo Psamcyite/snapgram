02ecb11ffd714fdbfc8d6de4f6df7ac49d628cf1
Psamcyite <amojoayokunle.com>
2024-03-04 21:39:15 -0600

    initial add deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDom from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDom.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
 )
